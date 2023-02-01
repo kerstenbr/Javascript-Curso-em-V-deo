f1cd925489c8c58a24c83dd0a0832f9c3aef59f2
Guilherme <84931896+kerstenbr@users.noreply.github.com>
2023-02-01 19:38:37 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você tomou uma multa por andar a ${vel}Km/h!`)
+} else {
+    console.log(`Tudo certo chefia!`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,12 @@
+var pais = 'EUA'
+var grana = null
+console.log(`Venho do ${pais}`)
+if (pais == 'Brasil') {
+    console.log('BR BR BR !!!!!!!')
+    grana = grana + 10000
+    console.log(`Pega pega essa grana meu irmão: ${grana} reais `)
+} else {
+    console.log('Gringo filho da puta some')
+    grana = 0
+    console.log(`Morra, perdeu sua grana: ${grana} reais `)
+}

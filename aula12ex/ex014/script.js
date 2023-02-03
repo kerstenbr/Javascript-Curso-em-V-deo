01519db6e94d5a271a197ee6f40d7a81be0a4185
Guilherme <84931896+kerstenbr@users.noreply.github.com>
2023-02-03 13:55:23 -0300

    TODO: Arrumar o relogio
    O relogio funciona, mas o jeito que eu fiz funcionar me deixa com nojo de mim mesmo.

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,45 @@
+//Isso aqui é uma gambiarra malfeita.
+//TODO: Realmente dar refresh na div em vez de fazer essa coisa porca
+function carregar(){
+    setInterval(carregar_teste, 1000)
+}
+
+//A Função em si
+function carregar_teste(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    let min = data.getMinutes()
+    let sec = data.getSeconds()
+
+    //Outras 2 gambiarras porcas
+    //TODO: Melhorar isso e fazer as coisas de um modo descente
+    if (min < 10){
+        min = '0' + min
+    }
+    
+    if (sec < 10){
+        sec = '0' + sec
+    }
+    
+   
+    if (hora >= 0 && hora < 12){
+        // BOM DIA
+        msg.innerHTML = `Agora são ${hora}:${min}:${sec} da manhã`
+        img.src = 'img/manha.jpg'
+        document.body.style.background = 'rgb(245, 149, 66)'
+    } else if (hora >= 12 && hora <= 18){
+        // BOA TARDE
+        msg.innerHTML = `Agora são ${hora}:${min}:${sec} da tarde`
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = 'rgb(240, 87, 67)'
+    } else {
+        // BOA NOITE
+        msg.innerHTML = `Agora são ${hora}:${min}:${sec} da noite` 
+        img.src = 'img/noite.jpg'
+        document.body.style.background = 'rgb(46, 44, 69)'
+    }
+    
+}
+

6055ea9d0bfaef208a6f7313b26febc78813bc14
kerstenbr <kersten.kdasilva@gmail.com>
2023-02-07 18:29:59 -0300

    terminei

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -13,14 +13,55 @@ function verificar(){
         var idade = ano - Number(fAno.value)
         //res.innerHTML = `Idade Calculada: ${idade}`
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
 
         if (fSex[0].checked){
             genero = 'Homem'
+            if (idade <= 15){
+                //criança
+                img.setAttribute('src', 'img/homem crianca.jpg')
+                document.body.style.background = 'rgb(74, 107, 144)'
+            }else if (idade <= 59){
+                //adulto
+                img.setAttribute('src', 'img/homem adulto.jpg')
+                document.body.style.background = 'rgb(54, 50, 139)'
+            }else if (idade <= 105){
+                //velho
+                img.setAttribute('src', 'img/homem velho.jpg')
+                document.body.style.background = 'rgb(27, 23, 91)'
+            }else{
+                //darklord
+                img.setAttribute('src', 'img/demon king.jpg')
+                document.body.style.background = 'rgb(18, 17, 18)'
+                
+            }
         } else if (fSex[1].checked){
             genero = 'Mulher'
+            if (idade <= 15){
+                //criança
+                img.setAttribute('src', 'img/mulher crianca.jpg')
+                document.body.style.background = 'rgb(165, 82, 195)'
+            }else if (idade <= 59){
+                //adulto
+                img.setAttribute('src', 'img/mulher adulta.jpg')
+                document.body.style.background = 'rgb(160, 61, 236)'
+            }else if (idade <= 105){
+                //velho
+                img.setAttribute('src', 'img/mulher velha.jpg')
+                document.body.style.background = 'rgb(86, 23, 134)'
+            }else{
+                //darklord
+                img.setAttribute('src', 'img/demon king.jpg')
+                document.body.style.background = 'rgb(18, 17, 18)'
+                
+            }
+            
         }
         //res.style.textAlign = 'center' //Usar isso para o texto ficar centralizado, nesse caso ja vou fazer isso com css
         res.innerHTML = `${genero} com ${idade} anos`
+        res.appendChild(img)
     }
     
 

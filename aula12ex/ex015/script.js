180ca89a362decbe8cb8ab046667840df139ddab
Guilherme Kersten da Silva <kersten.kdasilva@gmail.com>
2023-02-07 07:56:37 -0300

    Parei em 15:15 no video

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -4,15 +4,24 @@ function verificar(){
     var fAno = document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    console.log(fAno)
+    //console.log(fAno)
 
     if (fAno.value.length == 0 || Number(fAno.value) > ano){
         alert('ERRO AAAA')
     }else {
         var fSex = document.getElementsByName('radsex')
         var idade = ano - Number(fAno.value)
-        res.innerHTML = `Idade Calculada: ${idade}`
-    }
+        //res.innerHTML = `Idade Calculada: ${idade}`
+        var genero = ''
 
+        if (fSex[0].checked){
+            genero = 'Homem'
+        } else if (fSex[1].checked){
+            genero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `${genero} com ${idade}`
+    }
+    
 
 }
